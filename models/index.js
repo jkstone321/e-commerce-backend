b3896d49e6d71050b49b7440f7fe9896e1141d56
jkstone321 <jkstone321@gmail.com>
2022-05-04 17:47:53 -0400

    added code for the models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'ProductTag',
+    unique: false
+  }
+}),
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'ProductTag',
+    unique: false
+
+  }
+})
 
 module.exports = {
   Product,
